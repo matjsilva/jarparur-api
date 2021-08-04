3e25b320f61ddd3fcdf1325dc78f3a92c308be1f
matjs <matheusjgsilva3530@gmail.com>
2021-08-04 13:46:09 -0300

    /classes

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const app = require('express')();
 const PORT = process.env.PORT || 3000;
 
 supportedLanguages = ['pt-BR']
-apiAllUrls = ['/api', '/api/classes', '/api/races', '/api/regions', '/api/empires', '/api/religions', '/api/spells']
+apiAllUrls = [
+    '/api', 
+    '/api/classes', 
+    '/api/races', 
+    '/api/dimensions', 
+    '/api/worlds',
+    '/api/regions',
+    '/api/empires',
+    '/api/factions',
+    '/api/heroes',
+    '/api/relics', 
+    '/api/religions', 
+    '/api/spells']
 supportedUrls = ['/api']
 
+// /api
 apiData = {
     'name': 'Jarparur API',
     'version': '1.0.0',
@@ -13,13 +26,23 @@ apiData = {
     'urls': supportedUrls
 }
 
+// /api/classes
+apiClassesData = {
+    'all': ['warrior', 'bjoreten', 'barbarian', 'ranger', 'mage', 'paladin', 'cleric', 'monk', 'bard', 'witch', 'assassin']
+}
+
 app.get("", (req, res) => {
     res.send("JARPARUR API\nby matjs");
 });
 
-
+// Visão Geral da API
 app.get("/api", (req, res) => {
     res.send(apiData);
+});
+
+// Classes
+app.get("/api/classes", (req, res) => {
+    res.send(apiClassesData);
 });
 
 app.listen(PORT, () => {
