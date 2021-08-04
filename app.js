faf601d8920c00b1b7afe45a1250cd361c539bdb
matjs <matheusjgsilva3530@gmail.com>
2021-08-04 13:02:40 -0300

    test

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const app = require('express')();
 const PORT = process.env.PORT || 3000;
 
+jarparur = {
+    'name': 'jarparur'
+}
+
 app.get("", (req, res) => {
     res.send("EAIKkKkKkK");
+});
+
+
+app.get("/api", (req, res) => {
+    res.send(jarparur);
 });
 
 app.listen(PORT, () => {
