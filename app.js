78297af9e4172e901dc5f2e2a194ff70e6498a4e
matjs <matheusjgsilva3530@gmail.com>
2021-08-05 14:46:01 -0300

    update routes

--- app.js
+++ app.js
@@ -1,41 +1,8 @@
 const app = require('express')();
+const fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
-// data
-supportedLanguages = ['pt-BR']
-apiAllUrls = [
-    '/api', 
-    '/api/classes', 
-    '/api/races', 
-    '/api/dimensions', 
-    '/api/worlds',
-    '/api/regions',
-    '/api/empires',
-    '/api/factions',
-    '/api/heroes',
-    '/api/relics', 
-    '/api/religions', 
-    '/api/spells']
-supportedUrls = ['/api', '/api/classes']
-
-// /api
-apiData = {
-    'name': 'Jarparur API',
-    'version': '1.0.0',
-    'author': 'matjs',
-    'languages': supportedLanguages,
-    'urls': supportedUrls
-}
-// /api/classes
-apiClassesData = {
-    'all': ['warrior', 'bjoreten', 'barbarian', 'ranger', 'mage', 'paladin', 'cleric', 'monk', 'bard', 'witch', 'assassin']
-}
-
-// utils
-urlToDict = {
-    '/api/classes': apiClassesData,
-    '/api/classes/': apiClassesData
-}
+supportedUrls = JSON.parse(fs.readFileSync(`api.json`))['urls']
 
 app.get("", (req, res) => {
     apiHome = `JARPARUR API | by matjs | URLs = ${supportedUrls.join(', ')}`
@@ -43,22 +10,28 @@ app.get("", (req, res) => {
 });
 
 app.get("/api", (req, res) => {
-    res.send(apiData)
+    let data = JSON.parse(fs.readFileSync('api.json'));
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(data, null, 4));
 });
 
 app.get("/api/:topic/", (req, res) => {
-    if(urlToDict[req.path] != undefined){
-        res.send(urlToDict[req.path]);   
-    } else{
+    try {
+        let data = JSON.parse(fs.readFileSync(`api/${req.params['topic']}.json`))
+        res.header("Content-Type",'application/json');
+        res.send(JSON.stringify(data, null, 4));
+    } catch (error) {
         res.send(`A URL inserida (${req.path}) não existe. URLs disponíveis:  ${supportedUrls.join(', ')}`)
     }
 });
 
 app.get("/api/:topic/:subTopic/", (req, res) => {
-    if(urlToDict[req.path] != undefined){
-        res.send(urlToDict[req.path]);
-    } else{
-        res.send(`A URL inserida (${req.path}) não existe. URLs disponíveis: ${supportedUrls.join(', ')}`)
+    try {
+        let data = JSON.parse(fs.readFileSync(`api/${req.params['topic']}/${req.params['subTopic']}.json`))
+        res.header("Content-Type",'application/json');
+        res.send(JSON.stringify(data, null, 4));
+    } catch (error) {
+        res.send(`A URL inserida (${req.path}) não existe. URLs disponíveis:  ${supportedUrls.join(', ')}`)
     }
 });
 
